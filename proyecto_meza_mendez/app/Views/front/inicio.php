db77d608d4f5022bb88a53066780b251b2cbee8e
lau <lautaromezagc@gmail.com>
2025-06-17 19:55:50 -0300

    nuevo frontend

--- proyecto_meza_mendez/app/Views/front/inicio.php
+++ proyecto_meza_mendez/app/Views/front/inicio.php
@@ -1,84 +1,57 @@
-<div id="bienvenido" class="text-center text-bg-primary p-3">	
-	<div class="row">
-		<div class="col" >
-			<h1><strong>Bienvenido/a a nuestro sitio.</strong></h1>
-			<p>Somos Carpijuegos, una empresa de desarrollo de videojuegos, situada en Argentina.</p>
-		</div>
-		<div class="col">
-			<img src="../proyecto_meza_mendez/assets/img/logo.png" class="rounded-4" width="200rem" height="150rem">
-		</div>
-	</div>  
-</div>
-
-<div id="sobrenos">
-	<h1 class="text-center" style="margin-bottom: 30px;"><strong>Sobre Nosotros.</strong></h1>
-	<div class="row">
-		<div class="col">
-			<img src="../proyecto_meza_mendez/assets/img/peluso.jpg" class="rounded-4" width="100%" >
-		</div>
-		<div class="col" style="text-align: justify;">
-			<p><strong>Carpijuegos</strong> fue fundada como empresa independiente en 2014 desde Corrientes, Argentina, por Federico Pelusa y Martin Soboreo, con la intención de tener una salida comercial para sus proyectos creativos. En 2015, lanzamos <strong>"Sandwhich Delivery"</strong>, nuestro primer juego de manera totalmente independiente, en ese entonces bajo el nombre <strong>PelusoGames</strong>. Queriendo representar nuestra natividad mas prominentmente, decidimos renombrar la compañía a <strong>CarpiJuegos</strong>, basandonos en el querido animal de la zona que hicimos nuestro logo. Desde entonces, hemos finalizado mas de 20 proyectos, tanto bajo contrato para otras empresas (<strong>Activision</strong>, <strong>Meta</strong>, entre otras) como de manera propia.</p>
-		</div>
-	</div>
-</div>  
-
-
-<div id="quehacemos" class="text-center text-bg-secondary p-3">
-<br>
-  <h1><strong>¿Que hacemos?</strong></h1>
-  <p>Desarrollamos juegos para plataformas moviles, consolas y PC. Algunos de nuestros títulos populares son: </p>
-
-  <!-- carrusel -->
-  <br>
-  <section class="small-middle-container">
-   <div id="carouselExample" class="carousel slide rounded">
-      <div class="carousel-inner">
-
-        <div class="carousel-item active">
-          <img src="../proyecto_meza_mendez/assets/img/juego1.jpg" class="d-block w-100" alt="El juego del Bicho.">
-        </div>
-
-        <div class="carousel-item">
-          <img src="../proyecto_meza_mendez/assets/img/juego2.jpg" class="d-block w-100" alt="11 vagos 1 pelota.">
-        </div>
-
-        <div class="carousel-item">
-          <img src="../proyecto_meza_mendez/assets/img/juego3.jpg" class="d-block w-100" alt="El juego del Bicho 2.">
-        </div>
-
-        <div class="carousel-item">
-          <img src="../proyecto_meza_mendez/assets/img/juego4.jpg" class="d-block w-100" alt="No. (Juego de Cartas)">
-        </div>
+<main id="pag-principal">
+  <section id="bienvenido" class="text-light text-bg-primary text-center p-3">
+    <h1 class="fw-bold ">¡Bienvenido al sitio de CarpiJuegos!</h1>
+    <h3>Somos una empresa de desarrollo de videojuegos, situada en Argentina.</h3>
+  </section>
+  <section id="sobrenos" class="text-center">
+    <h1 style="margin-bottom: 30px;"><b>Sobre Nosotros.</b></h1>
+    <div class="row">
+      <div class="col">
+        <img src="../proyecto_meza_mendez/assets/img/peluso.jpg" class=" mx-auto w-75 rounded-4" >
       </div>
-
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>  
+      <div class="col" style="text-align: justify;">
+        <p><b>Carpijuegos</b> fue fundada como empresa independiente en 2014 desde Corrientes, Argentina, por Federico Pelusa y Martin Soboreo, con la intención de tener una salida comercial para sus proyectos creativos. En 2015, lanzamos <b>"Sandwhich Delivery"</b>, nuestro primer juego de manera totalmente independiente, en ese entonces bajo el nombre <b>PelusoGames</b>. Queriendo representar nuestra natividad mas prominentmente, decidimos renombrar la compañía a <b>CarpiJuegos</b>, basandonos en el querido animal de la zona que hicimos nuestro logo. Desde entonces, hemos finalizado mas de 20 proyectos, tanto bajo contrato para otras empresas (<b>Activision</b>, <b>Meta</b>, entre otras) como de manera propia.</p>
+        <p>Conocé mas sobre nuestra historia <a href="./">acá</a></p>
+      </div>
     </div>
   </section>
-</div>
-
-<div id="quienessomos">
-  <h1 style="text-align:center;"><strong>¿Quienes somos?</strong></h1>
-  <br>
-  <div class="container text-center">
-    <div class="row">
-      <div class="col rounded-4 text-bg-warning" style="padding: 10px; margin: 10px;">
-        <img src="../proyecto_meza_mendez/assets/img/foto1.jpg" class="rounded-circle"  width="150px" height="150px">
-        <h2>Federico Pelusa</h2>
-        <p>Graduado de la UTN BA, Federico lleva 15 años en el area de produccion de aplicaciones de celulares, habiendo trabajado para Google, Meta y MercadoLibre, entre otras empresas. En la actualidad, se centra en tecnologias de multijuego cross-play</p>
-      </div>
-      <div class="col rounded-4 text-bg-success" style="padding: 10px; margin: 10px;">
-        <img src="../proyecto_meza_mendez/assets/img/foto2.jpg" class="rounded-circle" width="150px" height="150px">
-        <h2>Martin Saboreo</h2>
-        <p>Recibido de la UNNE, Martin se ha desenvuelto en el mundo de la tecnologia de forma independiente, enfocandose en el desarrollo de videojuegos para consolas. Recientemente, ha publicado estudios relacionados a la Realidad Virtual.</p>
-      </div>
+  <section id="quehacemos" class="text-center text-bg-secondary p-3">
+    <h3>Desarrollamos juegos para plataformas moviles, consolas y PC. Algunos de nuestros títulos populares son: </h3>
+    <!-- carrusel -->
+    <div id="carusel" class="carousel slide mx-auto w-75 p-5">
+      <div class="carousel-inner rounded-5 mh-100">
+          <div class="carousel-item active">
+          <img src="assets/img/juego1.jpg" class="mx-auto d-block mw-100 mh-100" alt="El juego del Bicho.">
+          </div>
+          <div class="carousel-item">
+          <img src="assets/img/juego2.jpg" class="mx-auto d-block mw-100 mh-100" alt="11 vagos 1 pelota.">
+          </div>
+          <div class="carousel-item">
+          <img src="assets/img/juego3.jpg" class="mx-auto d-block mw-100 mh-100" alt="El juego del Bicho 2.">
+          </div>
+          <div class="carousel-item">
+          <img src="assets/img/juego4.jpg" class="mx-auto d-block mw-100 mh-100" alt="No. (Juego de Cartas)">
+          </div>
+        </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carusel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bg-dark bg-opacity-50 rounded" aria-hidden="true"></span>
+        <span class="visually-hidden">Anterior</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carusel" data-bs-slide="next">
+        <span class="carousel-control-next-icon bg-dark bg-opacity-50 rounded" aria-hidden="true"></span>
+        <span class="visually-hidden">Siguiente</span>
+      </button>
     </div>
-  </div>
-</div>
+  </section>
+  <section id="resto" class="text-light text-bg-warning text-center p-5">
+    <h1>Visitá el resto de nuestra pagina:</h1>
+    <!-- datos -->
+    <div class="fs-4 p-5">
+        <a href="./quienes"><button class="btn btn-dark">Quienes Somos</button></a>
+        <a href="./contacto"><button class="btn btn-dark">Contacto</button></a>
+        <a href="./tienda"><button class="btn btn-dark">Tienda</button></a>
+        <a href="./historia"><button class="btn btn-dark">Historia</button></a>
+    </div>
+  </section>
+
+</main>
